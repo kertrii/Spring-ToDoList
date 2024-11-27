31e1df9cdcc74e3ae67536f28cffa5ed0f6d4b2d
Kateryna <kateryna299@gmail.com>
2024-11-27 22:40:32 +0100

    update: module 15

---
+++ src/main/java/org/example/todolist/controller/NoteController.java
@@ -0,0 +1,59 @@
+package org.example.todolist.controller;
+
+import org.example.todolist.model.Note;
+import org.example.todolist.service.NoteService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/note")
+public class NoteController {
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping
+    public String showHomePage() {
+        return "home-page";
+    }
+
+    @GetMapping("/list")
+    public String listNote(Model model) {
+        model.addAttribute("notes", noteService.listAll());
+        return "note-list";
+    }
+
+    @PostMapping("/delete")
+    public String deleteNote(@RequestParam("id") long id) {
+        noteService.deleteById(id);
+        return "redirect:/note/list";
+    }
+
+    @GetMapping("/edit")
+    public String editFromNote(@RequestParam("id") long id, Model model) {
+        Note note = noteService.getById(id);
+        model.addAttribute("note", note);
+        return "note-edit";
+    }
+
+    @PostMapping("/edit")
+    public String editNote(Note note) {
+        noteService.update(note);
+        return "redirect:/note/list";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("note", new Note());
+        return "create-note";
+    }
+
+    @PostMapping("/create")
+    public String createNote(@ModelAttribute Note note) {
+        noteService.add(note);
+        return "redirect:/note/list";
+    }
+}
